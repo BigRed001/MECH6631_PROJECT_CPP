17327f58e912926af11aa129cd3e5f24c8941567
Axel <axelfou45@gmail.com>
2026-05-05 03:01:47 -0400

    Format Waypoint.h with consistent indentation

--- Waypoint.h
+++ Waypoint.h
@@ -14,8 +14,7 @@ public:
         double v_max);
 };
 
-// Transform marker centroid to front-axle position (kinematic control point).
-// The front axle is AHEAD of the marker centroid by marker_offset_x pixels.
+
 inline void getWheelCenterPosition(double marker_x, double marker_y, double theta,
                                    double& wheel_x, double& wheel_y,
                                    double marker_offset_x = 31.0) {
